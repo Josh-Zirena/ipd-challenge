ce5e7ed826ddec70387ec6b39dd8135ea721f549
Josh <JoshZirena@gmail.com>
2020-07-15 16:17:44 +0200

    refactor: linter fix

--- supportTasks/processData.js
+++ supportTasks/processData.js
@@ -39,52 +39,52 @@ const handleDuplicateSKUs = function(rawData) {
         }
     });
 
-    console.log(SKUMap);
+    //console.log(SKUMap);
 
     // Merging units and sales
     [...SKUMap].forEach(([SKU, items]) => {
-    const max = {
-        highestSection: '',
-        highestSale: 0,
-        highestWeight: 0,
-    };
+        const max = {
+            highestSection: '',
+            highestSale: 0,
+            highestWeight: 0,
+        };
 
-    SKUMapGroupped.set(SKU, {});
+        SKUMapGroupped.set(SKU, {});
 
-    items.forEach((item) => {
-        Object.keys(item).forEach((key) => {
-        if (unitsRegx.test(key)) {
-            const units = Number.parseInt(item[key], 10);
+        items.forEach((item) => {
+            Object.keys(item).forEach((key) => {
+            if (unitsRegx.test(key)) {
+                const units = Number.parseInt(item[key], 10);
 
-            SKUMapGroupped.set(SKU, { 
-            ...SKUMapGroupped.get(SKU),
-            [key]: SKUMapGroupped.get(SKU).hasOwnProperty(key) ? SKUMapGroupped.get(SKU)[key] + units : units,
-            });
-        } else if (salesRegx.test(key)) {
-            const { groups: { year, month } } = key.match(salesRegx);
-            const weight = (Number.parseInt(year, 10) * 12) + Number.parseInt(month, 10);
-            const sales = Number.parseFloat(item[key]);
-            const isLatestMonth = weight === max.highestWeight && sales > max.highestSale;
+                SKUMapGroupped.set(SKU, { 
+                    ...SKUMapGroupped.get(SKU),
+                    [key]: SKUMapGroupped.get(SKU).hasOwnProperty(key) ? SKUMapGroupped.get(SKU)[key] + units : units,
+                });
+            } else if (salesRegx.test(key)) {
+                const { groups: { year, month } } = key.match(salesRegx);
+                const weight = (Number.parseInt(year, 10) * 12) + Number.parseInt(month, 10);
+                const sales = Number.parseFloat(item[key]);
+                const isLatestMonth = weight === max.highestWeight && sales > max.highestSale;
 
-            if (weight > max.highestWeight || isLatestMonth) {
-            max.highestSale = sales;
-            max.highestSection = item.Section;
-            max.highestWeight = weight;
+                if (weight > max.highestWeight || isLatestMonth) {
+                max.highestSale = sales;
+                max.highestSection = item.Section;
+                max.highestWeight = weight;
+                }
+
+                SKUMapGroupped.set(SKU, {
+                ...SKUMapGroupped.get(SKU),
+                [key]: SKUMapGroupped.get(SKU).hasOwnProperty(key) ? SKUMapGroupped.get(SKU)[key] + sales : sales,
+                });
             }
-
-            SKUMapGroupped.set(SKU, {
-            ...SKUMapGroupped.get(SKU),
-            [key]: SKUMapGroupped.get(SKU).hasOwnProperty(key) ? SKUMapGroupped.get(SKU)[key] + sales : sales,
             });
-        }
         });
-    });
 
-    SKUMapGroupped.set(SKU, {
-        SKU,
-        Section: max.highestSection,
-        ...SKUMapGroupped.get(SKU),
-    });
+        SKUMapGroupped.set(SKU, {
+            SKU,
+            Section: max.highestSection,
+            ...SKUMapGroupped.get(SKU),
+        });
     });
 
     const final = [...SKUMapGroupped].map(([_, mergedItem]) => mergedItem);
